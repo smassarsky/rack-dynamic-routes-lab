475803d4b888a8b005ee730e03802353d311e172
smassarsky <seth.massarsky@gmail.com>
2020-11-04 15:53:45 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+require 'rack'
+require 'pry'
+
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last
+      binding.pry
+      if @@items.map{|item| item.name}.include?(item)
+        resp.write @@items.find{|x| x.name == item}.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
